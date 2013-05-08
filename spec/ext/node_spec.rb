253541f7c7bb3f354e71c9ffcc724e89bb2c0484
Taiki ONO <taiks.4559@gmail.com>
2013-05-09 02:53:47 +0900

    describe Node#each

--- spec/ext/node_spec.rb
+++ spec/ext/node_spec.rb
@@ -1,12 +1,16 @@
 require "spec_helper"
 
 describe Mecab::Ext::Node do
-  let(:generator) { double("generator").tap {|o| o.should_receive(:call) } }
+  let(:generator) { double("generator").tap {|o| o.should_receive(:call).and_return(nil) } }
   subject { described_class.new(generator) }
 
   describe "#each" do
     it "should call generator#call" do
-      subject.each {|e| }
+      subject.each {}
+    end
+
+    it "should return self" do
+      expect(subject.each {}).to equal subject
     end
   end
 end
