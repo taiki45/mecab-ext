cec6e43a2ae32b42f64571a6dc607b2a52d3d5bd
Taiki ONO <taiks.4559@gmail.com>
2013-05-09 01:00:18 +0900

    describe and add Ext::Parser#parse

--- lib/mecab/ext/parser.rb
+++ lib/mecab/ext/parser.rb
@@ -3,22 +3,25 @@ require "active_support/core_ext/module/delegation"
 module MeCab
   module Ext
     class Parser
-
-      delegate :parse, :parseToNode, :parseString, :parseNBestInit, :to => :__tagger__
+      attr_reader :__tagger__
+      delegate :parseString, :parseNBestInit, :to => :__tagger__
 
       def initialize(opt = {})
-        @tagger = MeCab.Tagger.new
+        @__tagger__ = MeCab::Tagger.new
       end
 
-      def __tagger__
-        @tagger
+      def parse(str)
+        Node.new(@__tagger__.parseToNode(str))
       end
+
+      alias :parseToNode :parse
+      alias :parse_to_node :parse
     end
 
     class Node
-      include Enumurable
+      include Enumerable
 
-      def initialize(orginal)
+      def initialize(original)
         @nodes = original
       end
 
--- spec/ext/parser_spec.rb
+++ spec/ext/parser_spec.rb
@@ -3,11 +3,27 @@ require 'spec_helper'
 describe MeCab::Ext::Parser do
   let(:tagger) { double("tagger") }
 
-  %w(parse parseToNode parseString parseNBestInit).each do |name|
+  %w(parseString parseNBestInit).each do |name|
     it "shoud delegate #{name} to MeCab::Tagger instance" do
       tagger.should_receive(name.to_sym)
       subject.instance_variable_set(:@__tagger__, tagger)
       subject.send(name)
     end
   end
+
+  shared_context "with tagger mock", tagger: :mock do
+    let(:instance) { described_class.new }
+    before do
+      tagger.should_receive(:parseToNode)
+      instance.instance_variable_set(:@__tagger__, tagger)
+    end
+  end
+
+  describe "#parse", tagger: :mock do
+    subject { instance }
+    it "shoud call Tagger#parseToNode" do
+      subject.parse("test string")
+    end
+  end
+
 end
