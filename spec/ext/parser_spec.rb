5d84e1434f9f2d4c782769d98407290a67251ea6
Taiki ONO <taiks.4559@gmail.com>
2013-05-09 01:09:34 +0900

    describe Ext::Parser#parseToNode and #parse_to_node

--- spec/ext/parser_spec.rb
+++ spec/ext/parser_spec.rb
@@ -12,17 +12,26 @@ describe MeCab::Ext::Parser do
   end
 
   shared_context "with tagger mock", tagger: :mock do
-    let(:instance) { described_class.new }
+    subject { described_class.new }
     before do
       tagger.should_receive(:parseToNode)
-      instance.instance_variable_set(:@__tagger__, tagger)
+      subject.instance_variable_set(:@__tagger__, tagger)
     end
   end
 
   describe "#parse", tagger: :mock do
-    subject { instance }
     it "shoud call Tagger#parseToNode" do
       subject.parse("test string")
+    end
+  end
+
+  %w(parseToNode parse_to_node).each do |name|
+    describe "##{name}" do
+      it { should be_respond_to name }
+
+      it "shoud same as #parse" do
+        expect(subject.method(name)).to eq subject.method(:parse)
+      end
     end
   end
 
