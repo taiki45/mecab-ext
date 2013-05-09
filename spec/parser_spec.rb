59bdd2d45169752b09c46d19dec806059a587d2c
Taiki ONO <taiks.4559@gmail.com>
2013-05-09 21:01:54 +0900

    describe Parser.parse

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -4,8 +4,28 @@ describe Mecab::Ext::Parser do
 
   describe ".parse" do
     subject { described_class.parse("test string") }
+
     it "should return Node instance" do
       expect(subject).to be_instance_of Mecab::Ext::Node
+    end
+
+    it "should call Node.new" do
+      Mecab::Ext::Node.stub(:new).and_return("called")
+      expect(subject).to eq "called"
+    end
+
+    it "should pass proc to Node#initialize" do
+      Mecab::Ext::Node.should_receive(:new) do |arg|
+        expect(arg).to be_instance_of Proc
+      end
+      subject
+    end
+
+    it "should pass callable obj" do
+      Mecab::Ext::Node.should_receive(:new) do |arg|
+        expect(arg).to be_respond_to :call
+      end
+      subject
     end
   end
 
