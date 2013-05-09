47b71af35eeaf76f8b1d9a23cb89b01073297bed
Taiki ONO <taiks.4559@gmail.com>
2013-05-09 21:43:28 +0900

    describe Node#each with node mocks

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,16 +1,35 @@
 require "spec_helper"
 
 describe Mecab::Ext::Node do
-  let(:generator) { double("generator").tap {|o| o.should_receive(:call).and_return(nil) } }
-  subject { described_class.new(generator) }
 
   describe "#each" do
-    it "should call generator#call" do
-      subject.each {}
+    context "with generator mock" do
+      let(:generator) { mock("generator").tap {|o| o.should_receive(:call).and_return(nil) } }
+      subject { described_class.new(generator) }
+
+      it "calls given generator's :call" do
+        subject.each {}
+      end
+
+      it "returns self" do
+        expect(subject.each {}).to equal subject
+      end
     end
 
-    it "should return self" do
-      expect(subject.each {}).to equal subject
+    context "with node mocks" do
+      let(:node) { mock("node").tap {|o| o.stub(:next).and_return(nil) } }
+      let(:parent_node) { mock("node").tap {|o| o.should_receive(:next).and_return(node) } }
+      let(:generator) { double("generator").tap {|o| o.stub(:call).and_return(parent_node) } }
+      subject { described_class.new(generator) }
+
+      it "calls node#next" do
+        subject.each {}
+      end
+
+      it " yields sub node" do
+        subject.each {|test| expect(test).to equal node }
+      end
     end
   end
+
 end
