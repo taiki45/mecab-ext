5404c3fcd9f1b48a4848abfff4aeba935979df54
Taiki ONO <taiks.4559@gmail.com>
2013-05-09 22:12:32 +0900

    describe Node#each with mecab node mocks

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -2,6 +2,27 @@ require "spec_helper"
 
 describe Mecab::Ext::Node do
 
+  shared_context %{with MeCab::Node like mock which given "test string"}, mecab: :node do
+    let(:first_node) do
+      n = mock("node").tap {|o| o.stub(:surface).and_return("") }
+      n.tap {|o| o.stub(:next).and_return(second_node) }
+    end
+    let(:second_node) do
+      n = mock("node").tap {|o| o.stub(:surface).and_return("test") }
+      n.tap {|o| o.stub(:next).and_return(third_node) }
+    end
+    let(:third_node) do
+      n = mock("node").tap {|o| o.stub(:surface).and_return("string") }
+      n.tap {|o| o.stub(:next).and_return(fourth_node) }
+    end
+    let(:fourth_node) do
+      n = mock("node").tap {|o| o.stub(:surface).and_return("") }
+      n.tap {|o| o.stub(:next).and_return(nil) }
+    end
+    let(:generator) { double("generator", call: first_node) }
+    let(:tests) { Array.new }
+  end
+
   describe "#each" do
     context "with generator mock" do
       let(:generator) { mock("generator").tap {|o| o.should_receive(:call).and_return(nil) } }
@@ -28,6 +49,24 @@ describe Mecab::Ext::Node do
 
       it " yields sub node" do
         subject.each {|test| expect(test).to equal node }
+      end
+    end
+
+    context "with mecab nodes", mecab: :node do
+      subject { described_class.new(generator) }
+
+      it "yields nodes" do
+        subject.each {|test| expect(test).to be_instance_of RSpec::Mocks::Mock }
+      end
+
+      it "yields 3 times" do
+        subject.each {|test| tests.push test }
+        expect(tests).to have(3).yielded_items
+      end
+
+      it %{yields nodes that each node have "test", "string", ""} do
+        subject.each {|test| tests.push test.surface }
+        expect(tests).to eq ["test", "string", ""]
       end
     end
   end
