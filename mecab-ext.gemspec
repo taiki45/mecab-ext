17f618648d9085942b494092de31fae9940a34c1
Taiki ONO <taiks.4559@gmail.com>
2013-05-11 19:11:44 +0900

    add coveralls gem and setups for it

--- mecab-ext.gemspec
+++ mecab-ext.gemspec
@@ -24,4 +24,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake"
   spec.add_development_dependency "rspec", "~> 2.13.0"
   spec.add_development_dependency "simplecov", "~> 0.7.1"
+  spec.add_development_dependency "coveralls", "~> 0.6.7"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,11 @@ end
 $LOAD_PATH.unshift File.expand_path("../", __FILE__)
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 
+require 'coveralls'
 require 'simplecov'
+
+Coveralls.wear!
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
 SimpleCov.start do
   add_filter 'spec'
 end
