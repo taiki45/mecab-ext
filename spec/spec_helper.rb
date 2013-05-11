c9e591dd67f759048a32530f3eb92bc79a0cf233
Taiki ONO <taiks.4559@gmail.com>
2013-05-11 19:02:11 +0900

    add simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,4 +9,9 @@ end
 $LOAD_PATH.unshift File.expand_path("../", __FILE__)
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 
+require 'simplecov'
+SimpleCov.start do
+  add_filter 'spec'
+end
+
 require 'mecab/ext'
